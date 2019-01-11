138ddd2a7d1eb340232c7cd3cfac905b269179f1
xujustinj <xu.justin.j@gmail.com>
2019-01-10 21:47:51 -0500

    Update content to present day

---
+++ content.js
@@ -0,0 +1,86 @@
+const normal = {
+    name:        'Justin Xu',
+    date:        'Hover over the icons above to see more...',
+    implements:  'A first-year computer science student who enjoys problem '
+                 + 'solving, music, and washing dishes.',
+    description: ''
+};
+
+hover(normal);
+
+
+const adetos = new Project(
+    'adetos',
+    'ADETOS',
+    'Apr 2017 - Nov 2018',
+    'Google Sheets, Google Script',
+    'Version 3.1 of a digital assistant for organizing double-elimination'
+    + 'tournaments. It supports the creation of arbitrarily many separate '
+    + 'brackets with up to 64 players each, distributing byes fairly between '
+    + 'teams. While running the tournament itself, it automatically '
+    + 'prioritizes matches to be played next, and collects the scores once '
+    + 'they are completed. The creation of ADETOS was motivated by a large-'
+    + 'scale table tennis tournament being planned by my high school, '
+    + 'requiring a similarly-scaled system for organizing it. ADETOS was co-'
+    + 'created with classmate Siddarth Narasimhan.',
+    'https://docs.google.com/spreadsheets/d/1JsttjsZt6USt7h1Z66PhwibUmafxuH1Mwk'
+    + 'zk2tNlDag/edit?usp=sharing',
+    'adetos.png'
+);
+
+const hexagonal_2048 = new Project(
+    'hexagonal_2048',
+    'Hexagonal 2048',
+    'Jul 2017 -',
+    'JavaScript, p5.js',
+    'Made for the final project in an introductory game design course, this '
+    + 'game applies the rules of the popular game 2048 to a hexagonal grid. '
+    + 'This project was recently reopened to clean up the code and add new '
+    + 'features. Use the QWEASD keys to control the board.',
+    'https://xujustinj.github.io/Hexagonal-2048/',
+    '2048.png'
+);
+
+const ulam = new Project(
+    'ulam',
+    'A Geometric Explanation for Prime-rich Lines on the Ulam Spiral',
+    'Nov 2016 - Feb 2018',
+    'LaTeX, Java',
+    'For the IB Extended Essay, I wrote a mathematics paper that ended up '
+    + 'requiring me to greatly overstep my mathematical and technical ability '
+    + 'at the time of writing it. As a result of my ambition, I achieved both '
+    + 'a high mark and a kickstart in learning modular arithmetic and LaTeX.',
+    'https://xujustinj.github.io/Extended-Essay/',
+    'ulam.png'
+);
+
+const water = new Project(
+    'water',
+    'Water',
+    'May 2015 -',
+    'LMMS, Musescore',
+    'Having enjoyed electronic music since middle school, I made it my IB '
+    + 'Personal Project to learn to use software to become a great producer - '
+    + 'a goal which I ultimately fell short on, lacking sufficient technical '
+    + 'skill and knowledge. The songs I made have become some of my fondest '
+    + 'high school nostalgia.',
+    'https://soundcloud.com/watered-music',
+    'water.png'
+);
+
+const concertino = new Project(
+    'concertino',
+    'Concertino',
+    'Dec 2016 - Mar 2017',
+    'Microsoft Excel, Visual Basic for Applications',
+    'This spreadsheet was created for a grade 11 information technology '
+    + 'course. I consulted my piano teacher as my client, and designed a '
+    + 'scheduling tool to suit his needs. It needed to contain a database and '
+    + 'also display the schedule in a calendar. At the time I figured that I '
+    + 'could use VBA to set up macros and forms to emulate MS Access in Excel, '
+    + 'where I could display the calendar on a sheet. In hindsight, it was '
+    + 'probably better off building Concertino in Access and using VBA to '
+    + 'emulate Excel instead.',
+    'concertino/Concertino 1.1.1.xlsm',
+    'concertino.png'
+);
--- events.js
+++ events.js
@@ -1,39 +1,31 @@
-function Project() {
-    this.name;
-    this.date;
-    this.implements;
-    this.description;
-}
-
-var normal = new Project();
-normal.name = "Justin's Projects";
-normal.date = 'December 2017';
-normal.implements = 'HTML, CSS';
-normal.description = "Hello World! My name is Justin Xu. This site was created to be a collection of my prouder digital projects. This is not the complete set, but it showcases the variety of my digital experiences, which does not only include programming. Digital media or anything I can digitize is also included here.";
-
-var hexagonal_2048 = new Project();
-hexagonal_2048.name = 'Hexagonal 2048';
-hexagonal_2048.date = 'July 2017';
-hexagonal_2048.implements = 'JavaScript, p5.js';
-hexagonal_2048.description = 'This game applies the rules of the popular game 2048 to a hexagonal grid, rather than a square grid. It uses primarily JavaScript and the p5 library to edit an HTML canvas element that contains the game. This is not the first time someone has made a hexagonal variant of 2048. Nonetheless, this project was constructed from scratch and without consultation of existing versions. Use the QWEASD keys to control the board.';
-
-var water = new Project();
-water.name = 'Water';
-water.date = '2015 -';
-water.implements = 'LMMS';
-water.description = 'Starting in Grade 9, I dabbled occasionally in digital music production using LMMS. I am far from mastering it, but it has grown into a hobby that I am nearing the point where I would be proud of its results. This page contains some of my completed productions (the older of which I am not very proud of). Between these releases are many more incomplete projects.';
-
-var concertino = new Project();
-concertino.name = 'Concertino';
-concertino.date = 'December 2016 - March 2017';
-concertino.implements = 'Microsoft Excel, Visual Basic';
-concertino.description = 'This spreadsheet was created for the Internal Assessment in the ITGS course I took in Grade 11. I consulted my former piano teacher as my client, and designed a scheduling tool to suit his needs. The tool needed to contain a database, but Microsoft Access could not be used, since the tool also needed to contain a dynamic timetable. Under the folly that a macro-based database-spreadsheet would function just as well as standalone software, Concertino was born.';
-
 function hover(project) {
-    project_name.innerHTML = '<h1>' + project.name + '</h1>';
-    project_date.innerHTML = '<h2>' + project.date + '</h2>';
-    project_implements.innerHTML = '<h2>' + project.implements + '</h2>';
-    project_description.innerHTML = '<p>' + project.description + '</p>';
+    project_name.textContent = project.name;
+    project_date.textContent = project.date;
+    project_implements.textContent = project.implements;
+    project_description.textContent = project.description;
 }
 
-hover(normal);
+
+function Project(id, name, date, implements, description, link, img) {
+
+    this.name = name;
+    this.date = date;
+    this.implements = implements;
+    this.description = description;
+
+    let tile = document.createElement('a');
+    tile.setAttribute('class',       'tile');
+    tile.setAttribute('onmouseover', 'hover(' + id + ');');
+    tile.setAttribute('onmouseout',  'hover(normal);');
+    tile.setAttribute('href',        link);
+
+    tiles.appendChild(tile);
+
+    let image = document.createElement('img');
+    image.setAttribute('class', 'icon');
+    image.setAttribute('src',   'img/' + img);
+    image.setAttribute('alt',   name);
+
+    tile.appendChild(image);
+
+}
