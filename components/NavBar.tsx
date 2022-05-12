77756d5142e813fe019ca9ac47165790c1b35848
Justin Xu <xu.justin.j@gmail.com>
2022-05-12 01:14:57 -0700

    Implement custom app with dummy NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export default styled.div`
+  background-color: black;
+  color: white;
+  width: 100%;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -12,24 +12,13 @@ const GlobalStyle = createGlobalStyle`
 }
 `;
 
-const NavBar = styled.div`
-  background-color: black;
-  color: white;
-  width: 100%;
-`;
-
 export default function Home() {
   return (
     <>
+      <GlobalStyle />
       <Head>
         <title>Justin Xu</title>
       </Head>
-      <>
-        <GlobalStyle />
-        <NavBar>
-          <h1>Justin Xu</h1>
-        </NavBar>
-      </>
     </>
   );
 }
